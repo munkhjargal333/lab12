b733b120da821004b6e02ac658393ce8f75a2ab7
munkhjargal333 <munkhjargal.ts39@gmail.com>
2025-05-16 00:07:09 +0800

    spring boot pack v1

---
+++ src/test/java/com/example/lab12/Lab12ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.lab12;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab12ApplicationTests {
+
+	// @Test
+	// // void contextLoads() {
+	// // }
+
+}
